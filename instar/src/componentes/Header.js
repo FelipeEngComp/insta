15d2bd51d887a76d303d88d9a7a772cd12c4f3d7
FelipeEngComp <feliperibeiroalves@gmail.com>
2018-11-05 11:47:40 -0200

    fim do segundo modulo

--- instar/src/componentes/Header.js
+++ instar/src/componentes/Header.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import Pubsub from 'pubsub-js';
 
 export default class Header extends Component {
 
+    pesquisa(event){
+      event.preventDefault();
+      fetch('')
+      .then(response=>response.json())
+      .then(fotos =>{
+        Pubsub.publish('timeline',fotos);
+      })
+    }
+
     render(){
         return (
-        <header className="header container">
+          <header className="header container">
           <h1 className="header-logo">
             InstaGroup
           </h1>
 
-          <form className="header-busca">
-            <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo"/>
+          <form className="header-busca" onSubmit={this.pesquisa.bind(this)}>
+            <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo" ref={input => this.loginPesquisado = input}/>
             <input type="submit" value="Buscar" className="header-busca-submit"/>
           </form>
 
@@ -26,7 +36,7 @@ export default class Header extends Component {
               </li>
             </ul>
           </nav>
-        </header>            
+        </header>     
         );
     }
 }
